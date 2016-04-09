2fe5c06696e9572d3cf45531242e1096ca4e9465
Nivesh Singh <nivesh2@gmail.com>
2016-04-09 18:00:42 +0530

    Header and Footer working perfectly

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import Header from "./Header";
+import Footer from "./Footer";
+
 export default class Layout extends React.Component {
   render(){
     return (
       <div>
-
-        <h1>This is from React</h1>
-        <p>Hi, there!</p>
+          <Header title="TodoApp - react"/>
+          <Footer/>
       </div>
     );
   }
