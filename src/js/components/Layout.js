82beeba41a426921c9099192bf40f4e126459c83
Nivesh Singh <nivesh2@gmail.com>
2016-04-09 18:19:31 +0530

    input box working fine.

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,10 +4,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      title:"Welcome",
+    };
+  }
+
+  changeTitle(title){
+    this.setState({title});
+  }
+
   render(){
     return (
       <div>
-          <Header title="TodoApp - react"/>
+          <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
           <Footer/>
       </div>
     );
